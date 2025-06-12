6a104420b7b073fbd6522155acc2bb40b3b830fa
karankumar790 <karan20-cs@sanskar.org>
2025-06-12 15:28:37 +0530

    live

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,36 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
- 
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            // Split react/react-dom to a separate chunk
+            if (id.includes("react")) return "react-vendor"
+            // Split video libraries separately
+            if (
+              id.includes("react-player") ||
+              id.includes("wistia") ||
+              id.includes("youtube") ||
+              id.includes("mux") ||
+              id.includes("vimeo")
+            )
+              return "video-players"
+
+            return "vendor" // generic vendor chunk
+          }
+        },
+      },
     },
   },
 })
